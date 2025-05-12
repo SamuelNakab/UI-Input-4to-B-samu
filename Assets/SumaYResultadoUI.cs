47e03a39fe8119fdd1f79c4a8e81a85405b6063f
SamuelNakab <167198088+SamuelNakab@users.noreply.github.com>
2025-05-12 09:07:06 -0300

    Tp Terminado

---
+++ Assets/SumaYResultadoUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SumaYResultadoUI : MonoBehaviour
+{
+    public TMP_InputField InputNum1;
+    public TMP_InputField InputNum2;
+    public TextMeshProUGUI TXTresultado;
+    int num1;
+    int num2;
+
+
+    void Start()
+    {
+        TXTresultado.text = "";
+    }
+
+
+    public void SumarYMostrar()
+    {
+        num1 = int.Parse(InputNum1.text);
+        num2 = int.Parse(InputNum2.text);
+
+        TXTresultado.text = (num1 + num2).ToString();
+        Debug.Log("Suma Realizada");
+    }
+
+}
